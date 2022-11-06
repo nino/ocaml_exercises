d47bea9e0447756a873e3848416e69efa1d35b5d
Nino Annighoefer <nino@ninoan.com>
2022-11-06 16:25:16 +0000

    And now we're learning about first-class modules!

--- advent2015/advent2015.ml
+++ advent2015/advent2015.ml
@@ -1,10 +1,14 @@
-let call_and_flush f =
-  f ();
+module type AdventDay = sig
+  val run : unit -> unit
+end
+
+let call_and_flush (module M : AdventDay) =
+  M.run ();
   Stdio.printf "\n";
   Stdio.Out_channel.flush Stdio.Out_channel.stdout
 
 let () = Stdio.printf "Yo\n"
-let () = call_and_flush Day2.run
-let () = call_and_flush Day3.run
-let () = call_and_flush Day4.run
-let () = call_and_flush Day5.run
+let () = call_and_flush (module Day2)
+let () = call_and_flush (module Day3)
+let () = call_and_flush (module Day4)
+let () = call_and_flush (module Day5)
