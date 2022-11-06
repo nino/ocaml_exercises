e7740e43e61435a91e85e6a83b5de43420b21892
Nino Annighoefer <nino@ninoan.com>
2022-11-06 14:54:00 +0000

    Format

--- advent2015/day4.ml
+++ advent2015/day4.ml
@@ -1,12 +1,12 @@
-let does_number_produce_valid_hash ?(extra_hard=false) secret_key number =
+let does_number_produce_valid_hash ?(extra_hard = false) secret_key number =
   let hash_input = secret_key ^ Int.to_string number in
   let hex = hash_input |> Md5_lib.string |> Md5_lib.to_hex in
-  String.is_prefix hex
-    ~prefix:(if  extra_hard then "000000" else "00000")
+  String.is_prefix hex ~prefix:(if extra_hard then "000000" else "00000")
 
 let mine_coin ?(extra_hard = false) secret_key =
   let rec loop counter =
-    if does_number_produce_valid_hash ~extra_hard secret_key counter then counter
+    if does_number_produce_valid_hash ~extra_hard secret_key counter then
+      counter
     else loop (counter + 1)
   in
   loop 1
