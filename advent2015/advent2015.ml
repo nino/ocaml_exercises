0d37b407e01766ac5eb8ed97c2eccd3abf23f9f1
Nino Annighoefer <nino@ninoan.com>
2022-11-06 16:17:09 +0000

    Day 5

--- advent2015/advent2015.ml
+++ advent2015/advent2015.ml
@@ -1,4 +1,9 @@
+let call_and_flush f =
+  f ();
+  Stdio.Out_channel.flush Stdio.Out_channel.stdout
+
 let () = Stdio.printf "Yo\n"
-let () = Day2.run ()
-let () = Day3.run ()
-let () = Day4.run ()
+let () = call_and_flush Day2.run
+let () = call_and_flush Day3.run
+let () = call_and_flush Day4.run
+let () = call_and_flush Day5.run
---
+++ advent2015/day5.ml
@@ -0,0 +1,68 @@
+let at_least_3_vowels string =
+  let num_vowels =
+    String.count string ~f:(function
+      | 'a' | 'e' | 'i' | 'o' | 'u' -> true
+      | _ -> false)
+  in
+  num_vowels >= 3
+
+let at_least_one_double string =
+  let len = String.length string in
+  let idx = ref 0 in
+  while !idx < len - 1 && Char.(string.[!idx] <> string.[!idx + 1]) do
+    Int.incr idx
+  done;
+  !idx < len - 1
+
+let no_forbidden_seqs string =
+  List.for_all [ "ab"; "cd"; "pq"; "xy" ] ~f:(fun substring ->
+      not (String.is_substring ~substring string))
+
+let is_nice1 string =
+  at_least_3_vowels string && at_least_one_double string
+  && no_forbidden_seqs string
+
+let sexp_of_set (s : (string, String.comparator_witness) Set.t) =
+  List.sexp_of_t String.sexp_of_t (Set.to_list s)
+
+let has_nonoverlapping_double_pair string =
+  let len = String.length string in
+  let rec loop prev_pairs current_pairs index =
+    if index = len - 1 then false
+    else
+      let next_pair =
+        String.of_char_list [ string.[index]; string.[index + 1] ]
+      in
+      if Set.mem prev_pairs next_pair then true
+      else loop current_pairs (Set.add current_pairs next_pair) (index + 1)
+  in
+  let empty_set = Set.empty (module String) in
+  loop empty_set empty_set 0
+
+let has_xyx_pattern string =
+  let len = String.length string in
+  let idx = ref 0 in
+  while !idx < len - 2 && Char.(string.[!idx] <> string.[!idx + 2]) do
+    Int.incr idx
+  done;
+  !idx < len - 2
+
+let is_nice2 string =
+  has_nonoverlapping_double_pair string && has_xyx_pattern string
+
+let test_nice_2 s should_be =
+  Stdio.printf "“%s” should be %s, and is: %s\n" s should_be
+    (if is_nice2 s then "nice" else "not nice")
+
+let run () =
+  Stdio.printf "Advent of code 2015, day 5!\n";
+
+  let input_path = Utils.input_path_for_day 5 in
+  let inputs = Stdio.In_channel.read_lines input_path in
+
+  let number_of_nice = List.count inputs ~f:is_nice1 in
+  Stdio.printf "There are %d nice strings.\n" number_of_nice;
+
+  let number_of_nice_new = List.count inputs ~f:is_nice2 in
+  Stdio.printf "Under the new system, there are %d nice strings.\n"
+    number_of_nice_new
