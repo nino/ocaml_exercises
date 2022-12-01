e073742767101c6d69778b3b83f28d84ad29864d
Nino Annighoefer <nino@ninoan.com>
2022-12-01 15:50:58 +0000

    2022 Day 1, part 2

--- advent2022/day1.ml
+++ advent2022/day1.ml
@@ -11,10 +11,13 @@ let run () =
     |> List.map ~f:(fun group ->
            String.split ~on:'\n' group |> List.map ~f:parse_int_or_zero)
   in
-  let biggest_elf_storage =
-    elves
-    |> List.map ~f:(List.fold ~init:0 ~f:( + ))
-    |> List.fold ~init:0 ~f:Int.max
-  in
+  let elves = elves |> List.map ~f:(List.fold ~init:0 ~f:( + )) in
+  let biggest_elf_storage = elves |> List.fold ~init:0 ~f:Int.max in
   printf "The elf with the most calories has %d calories.\n" biggest_elf_storage;
+  let sorted_elves = List.sort elves ~compare:(fun a b -> -Int.compare a b) in
+  let biggest_3_elves_together =
+    List.take sorted_elves 3 |> List.fold ~init:0 ~f:( + )
+  in
+  printf "The biggest 3 elves have %d calories together.\n"
+    biggest_3_elves_together;
   ()
