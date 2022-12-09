aea81d41cb969a4e2ea596c77b26d6d666d4a860
Nino Annighoefer <nino@ninoan.com>
2022-12-09 17:00:05 +0000

    2022 day 8 part 2

--- advent2022/forest.mli
+++ advent2022/forest.mli
@@ -4,3 +4,8 @@ type t
 
 val of_string_list : string list -> (t, Error.t) Result.t
 val count_visible : t -> int
+val highest_scenic_score : t -> int
+val trees_visible_left : t -> int -> int -> int
+val trees_visible_right : t -> int -> int -> int
+val trees_visible_top : t -> int -> int -> int
+val trees_visible_bottom : t -> int -> int -> int
