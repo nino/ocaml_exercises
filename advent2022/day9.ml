36be4ac741c9a4b23c98ded37de192bc7224b95e
Nino Annighoefer <nino@ninoan.com>
2022-12-11 21:33:06 +0000

    2022 day 9 refactor

--- advent2022/day9.ml
+++ advent2022/day9.ml
@@ -3,43 +3,30 @@ open Stdio
 
 let day = 9
 
-module Direction = struct
-  type t = Up | Down | Left | Right
-
-  let of_char = function
-    | 'U' -> Up
-    | 'D' -> Down
-    | 'L' -> Left
-    | 'R' -> Right
-    | _ -> failwith "Invalid direction."
-end
-
 module Point = struct
   module T = struct
     type t = int * int [@@deriving compare, sexp_of, equal]
 
-    let move (x, y) direction =
-      match direction with
-      | Direction.Up -> (x, y + 1)
-      | Direction.Down -> (x, y - 1)
-      | Direction.Left -> (x - 1, y)
-      | Direction.Right -> (x + 1, y)
+    let delta_of_char = function
+      | 'U' -> (0, 1)
+      | 'D' -> (0, -1)
+      | 'L' -> (-1, 0)
+      | 'R' -> (1, 0)
+      | _ -> failwith "Unrecognized direction"
 
-    let catch_up (x1, y1) (x2, y2) =
-      if x1 = x2 && y1 = y2 then (x1, y1)
-      else if x1 = x2 && y1 < y2 - 1 then (x1, y1 + 1)
-      else if x1 = x2 && y1 > y2 + 1 then (x1, y1 - 1)
-      else if y1 = y2 && x1 < x2 - 1 then (x1 + 1, y1)
-      else if y1 = y2 && x1 > x2 + 1 then (x1 - 1, y1)
-      else if (x1 < x2 - 1 && y1 < y2) || (x1 < x2 && y1 < y2 - 1) then
-        (x1 + 1, y1 + 1)
-      else if (x1 > x2 + 1 && y1 < y2) || (x1 > x2 && y1 < y2 - 1) then
-        (x1 - 1, y1 + 1)
-      else if (x1 < x2 - 1 && y1 > y2) || (x1 < x2 && y1 > y2 + 1) then
-        (x1 + 1, y1 - 1)
-      else if (x1 > x2 + 1 && y1 > y2) || (x1 > x2 && y1 > y2 + 1) then
-        (x1 - 1, y1 - 1)
-      else (x1, y1)
+    let move (x, y) (dx, dy) = (x + dx, y + dy)
+
+    let delta_to_catch_up (x1, y1) (x2, y2) =
+      if x1 = x2 && y1 = y2 then (0, 0)
+      else if x1 = x2 && y1 < y2 - 1 then (0, 1)
+      else if x1 = x2 && y1 > y2 + 1 then (0, -1)
+      else if y1 = y2 && x1 < x2 - 1 then (1, 0)
+      else if y1 = y2 && x1 > x2 + 1 then (-1, 0)
+      else if (x1 < x2 - 1 && y1 < y2) || (x1 < x2 && y1 < y2 - 1) then (1, 1)
+      else if (x1 > x2 + 1 && y1 < y2) || (x1 > x2 && y1 < y2 - 1) then (-1, 1)
+      else if (x1 < x2 - 1 && y1 > y2) || (x1 < x2 && y1 > y2 + 1) then (1, -1)
+      else if (x1 > x2 + 1 && y1 > y2) || (x1 > x2 && y1 > y2 + 1) then (-1, -1)
+      else (0, 0)
   end
 
   include T
@@ -47,9 +34,20 @@ module Point = struct
 end
 
 module Rope = struct
-  type t = { head : Point.t; tail : Point.t }
+  (* type t = Point.t list *)
 
-  let init () = { head = (0, 0); tail = (0, 0) }
+  let init ?(len = 2) () = List.init len ~f:(fun _ -> (0, 0))
+
+  let rec move t delta =
+    match t with
+    | [] -> []
+    | hd :: follow :: tl ->
+        let new_hd = Point.move hd delta in
+        let follow_delta = Point.delta_to_catch_up follow new_hd in
+        new_hd :: move (follow :: tl) follow_delta
+    | [ hd ] -> [ Point.move hd delta ]
+
+  let last t = List.last_exn t
 end
 
 let sequence_of_moves lines =
@@ -58,7 +56,7 @@ let sequence_of_moves lines =
       let iterations =
         String.sub line ~pos:2 ~len:(String.length line - 2) |> Int.of_string
       in
-      let direction = Direction.of_char line.[0] in
+      let direction = Point.delta_of_char line.[0] in
       Sequence.init iterations ~f:(fun _ -> direction))
 
 let tests () =
@@ -67,10 +65,9 @@ let tests () =
   let visited_points, _final_rope =
     Sequence.fold moves
       ~init:(Set.empty (module Point), Rope.init ())
-      ~f:(fun (visited_points, { head; tail }) direction ->
-        let new_head = Point.move head direction in
-        let new_tail = Point.catch_up tail new_head in
-        (Set.add visited_points new_tail, { head = new_head; tail = new_tail }))
+      ~f:(fun (visited_points, rope) delta ->
+        let new_rope = Rope.move rope delta in
+        (Set.add visited_points (Rope.last new_rope), new_rope))
   in
   printf "Test: The tail of the rope visits %d positions at least once.\n"
     (Set.length visited_points)
@@ -82,10 +79,9 @@ let run () =
   let visited_points, _final_rope =
     Sequence.fold moves
       ~init:(Set.empty (module Point), Rope.init ())
-      ~f:(fun (visited_points, { head; tail }) direction ->
-        let new_head = Point.move head direction in
-        let new_tail = Point.catch_up tail new_head in
-        (Set.add visited_points new_tail, { head = new_head; tail = new_tail }))
+      ~f:(fun (visited_points, rope) delta ->
+        let new_rope = Rope.move rope delta in
+        (Set.add visited_points (Rope.last new_rope), new_rope))
   in
   printf "The tail of the rope visits %d positions at least once.\n"
     (Set.length visited_points)
